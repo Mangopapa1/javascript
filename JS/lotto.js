5534eb9943af9092cd574621bd36145f24226332
Mangopapa1 <92600646+Mangopapa1@users.noreply.github.com>
2021-12-13 23:48:35 +0900

    Add files via upload

---
+++ JS/lotto.js
@@ -0,0 +1,12 @@
+Math.floor(Math.random()*5); //2 ~ 6
+
+Math.floor(Math.random()*5+2); // 2 ~ 7
+
+function randomNum(min, max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+let rotto = new Set();
+while(rotto.size < 6) {
+    rotto.add()
+}
